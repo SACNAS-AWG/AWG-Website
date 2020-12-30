727bbd71e3157270225857dfc63d3c45fc8036a3
Kyle <kyle_wong@ucsb.edu>
2020-12-29 22:51:40 -0800

    refactored into folders

---
+++ scripts/navbar.js
@@ -0,0 +1,11 @@
+const navbar_content = `
+    <div class="links">
+        <a href="index.html">Home</a>
+        <a href="about.html">About</a>
+        <a href="contact.html">Contact</a>
+    </div>
+`;
+
+const NavBar = document.getElementById("navbar");
+
+NavBar.innerHTML = navbar_content;
